853742f69a78198760a62b76fabe2ca364b6f953
Eliran Levi <eliran@stratoscale.com>
2015-07-15 12:16:58 +0300

    Use simple http requests instead of realtimewebui

--- py/rackattack/dashboard/main.py
+++ py/rackattack/dashboard/main.py
@@ -7,6 +7,9 @@ from realtimewebui import rootresource
 from realtimewebui import render
 from rackattack.dashboard import pollthread
 from twisted.web import static
+from twisted.web.resource import Resource
+import json
+import pymongo
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -17,6 +20,8 @@ parser.add_argument("--realtimewebuiRoot")
 parser.add_argument("--dashboardRoot")
 parser.add_argument("--localhostRackattackProvider", action='store_true')
 args = parser.parse_args()
+connection = pymongo.MongoClient("localhost")
+db = connection.rackattack_stats
 
 if args.realtimewebuiRoot is not None:
     realtimewebui.config.REALTIMEWEBUI_ROOT_DIRECTORY = args.realtimewebuiRoot
@@ -25,8 +30,37 @@ if args.localhostRackattackProvider:
         'tcp://localhost:1014@@amqp://guest:guest@localhost:1013/%2F@@http://localhost:1016'
 
 
-hostsStatsPoller = pollthread.PollThread()
-inaugurationTimePoller = pollthread.InaugurationTimesPollThread()
+def getAverageInaugurationTimeBy(fieldName):
+    pipe = [{'$match': {'inauguration_done': True}},
+            {'$group': {'_id': "$%(fieldName)s" % dict(fieldName=fieldName),
+                        'averageInaugurationTime': {'$avg': '$inauguration_period_length'}}},
+            {'$sort': {'averageInaugurationTime': -1}}]
+    data = db.inaugurations.aggregate(pipe)
+    if isinstance(data, dict):
+        data = data["result"]
+    else:
+        data = list(data)
+    return data
+
+
+class Query(Resource):
+    def render_GET(self, request):
+        request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+        if "by" in request.args:
+            groupByField = request.args["by"]
+            try:
+                groupByField = groupByField[0]
+                groupByField = dict(host="host_id", label="imageLabel")[groupByField]
+            except (IndexError, KeyError) as ex:
+                logging.warn("Got a request with an invalid 'by' field")
+                raise
+        else:
+            logging.warn("Got a request with an invalid 'by' field")
+            raise ValueError("Missing 'by' field (host/label)")
+        result = getAverageInaugurationTimeBy(groupByField)
+        return json.dumps(result)
+
+pollThread = pollthread.PollThread()
 
 render.addTemplateDir(os.path.join(args.dashboardRoot, 'html'))
 render.DEFAULTS['title'] = "Rackattack"
@@ -37,4 +71,5 @@ root.putChild("js", static.File(os.path.join(args.dashboardRoot, "js")))
 root.putChild("static", static.File(os.path.join(args.dashboardRoot, "static")))
 root.putChild("favicon.ico", static.File(os.path.join(args.dashboardRoot, "static", "favicon.ico")))
 root.putChild("inaugurations", rootresource.Renderer("inaugurations.html", {}))
+root.putChild("averageInaugurationTime", Query())
 server.runUnsecured(root, args.webPort, args.webSocketPort)
--- py/rackattack/dashboard/pollthread.py
+++ py/rackattack/dashboard/pollthread.py
@@ -3,7 +3,6 @@ from rackattack.dashboard import tojs
 from rackattack import clientfactory
 import time
 import logging
-import pymongo
 
 
 class PollThread(threading.Thread):
@@ -38,59 +37,3 @@ class PollThread(threading.Thread):
 
     def _publish(self, status):
         tojs.set('status', status)
-
-
-class InaugurationTimesPollThread(threading.Thread):
-    POLL_INTERVAL = 60
-
-    def __init__(self, databaseHost="localhost"):
-        threading.Thread.__init__(self)
-        self.databaseHost = databaseHost
-        self._connection = None
-        self._db = None
-        self.daemon = True
-        threading.Thread.start(self)
-        logging.info("Inauguration poll thread initialized.")
-
-    def run(self):
-        while True:
-            try:
-                self._connection = pymongo.MongoClient(self.databaseHost)
-                self._db = self._connection.rackattack_stats
-            except:
-                logging.exception("Unable to create database client")
-                time.sleep(self.POLL_INTERVAL)
-                continue
-            try:
-                self._work()
-            finally:
-                self._connection.close()
-                time.sleep(self.POLL_INTERVAL)
-
-    def getAverageInaugurationTimeBy(self, fieldName):
-        pipe = [{'$match': {'inauguration_done': True}},
-                {'$group': {'_id': "$%(fieldName)s" % dict(fieldName=fieldName),
-                            'averageInaugurationTime': {'$avg': '$inauguration_period_length'}}},
-                {'$sort': {'averageInaugurationTime': -1}}]
-        data = self._db.inaugurations.aggregate(pipe)
-        if isinstance(data, dict):
-            data = data["result"]
-        else:
-            data = list(data)
-        return data
-
-    def _work(self):
-        try:
-            while True:
-                logging.info("Querying database for inauguratio times...")
-                result = dict()
-                result["averageTimeByHost"] = self.getAverageInaugurationTimeBy("host_id")
-                result["averageTimeByLabel"] = self.getAverageInaugurationTimeBy("imageLabel")
-                print result["averageTimeByLabel"]
-                self._publish(result)
-                time.sleep(self.POLL_INTERVAL)
-        except:
-            logging.exception("Unable to query status")
-
-    def _publish(self, data):
-        tojs.set('inaugurationsStats', data)
